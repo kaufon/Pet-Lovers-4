15c0e0b806eda2dbd7abe81cb1c9984312ed443b
kaufon <kauanfvale@gmail.com>
2024-11-01 18:56:41 -0300

    ♻️refactor: dashboard

--- apps/web/src/ui/components/pages/dashboard/index.tsx
+++ apps/web/src/ui/components/pages/dashboard/index.tsx
@@ -1,27 +1,15 @@
 "use client";
-import React, { useState } from "react";
 import {
-  Button,
-  Table,
-  TableBody,
-  TableCell,
-  TableColumn,
-  TableHeader,
-  TableRow,
-  Tooltip,
+  Button
 } from "@nextui-org/react";
-import { DollarSign, Pen, Trash } from "lucide-react";
+import { useState } from "react";
+import { ClientTable } from "./clients-table";
 import { RegisterClientForm } from "./register-client-form";
-import { AlertModal } from "../../commons/alert-modal";
-import { ConsumeForm } from "./consume-form";
-import { useApi } from "@/src/ui/hooks/use-api";
 import { useDashboard } from "./use-dashboard";
 
 export const DashBoardPage = () => {
-  const data = useDashboard();
-  console.log(data);
+  const { clients, isLoading, deleteClient } = useDashboard();
   const [isModalOpen, setModalOpen] = useState(false);
-  const [isAlertModalOpen, setAlertModalOpen] = useState(false);
   const [isConsumeModalOpen, setConsumeModalOpen] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
 
@@ -35,9 +23,6 @@ export const DashBoardPage = () => {
     setIsEditing(false);
   };
 
-  const openAlertModal = () => setAlertModalOpen(true);
-  const closeAlertModal = () => setAlertModalOpen(false);
-
   const openConsumeModal = () => setConsumeModalOpen(true);
   const closeConsumeModal = () => setConsumeModalOpen(false);
 
@@ -46,43 +31,16 @@ export const DashBoardPage = () => {
       <Button color="default" onClick={() => openModal(false)}>
         Adicionar Cliente
       </Button>
-      <Table selectionMode="single" shadow="md">
-        <TableHeader>
-          <TableColumn>ID</TableColumn>
-          <TableColumn>Nome</TableColumn>
-          <TableColumn>CPF</TableColumn>
-          <TableColumn>Pets</TableColumn>
-          <TableColumn>Ações</TableColumn>
-        </TableHeader>
-        <TableBody>
-          <TableRow>
-            <TableCell>ID do Joaozinho</TableCell>
-            <TableCell>Nome do Joaozinho</TableCell>
-            <TableCell>Cpf do Joaozinho</TableCell>
-            <TableCell>Pets do Joaozinho</TableCell>
-            <TableCell>
-              <div className="flex flex-row gap-3 text-zinc-400">
-                <Tooltip content="Consumir">
-                  <DollarSign onClick={openConsumeModal} />
-                </Tooltip>
-                <Tooltip content="Editar">
-                  <Pen onClick={() => openModal(true)} />
-                </Tooltip>
-                <Tooltip content="Deletar">
-                  <Trash onClick={openAlertModal} />
-                </Tooltip>
-              </div>
-            </TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
+      <ClientTable
+        clients={clients}
+        isLoading={isLoading}
+        handleDeleteClient={deleteClient}
+      />
       <RegisterClientForm
         isOpen={isModalOpen}
         isClosed={closeModal}
         isRegister={!isEditing} // Register when not editing
       />
-      <ConsumeForm isOpen={isConsumeModalOpen} isClosed={closeConsumeModal} />
-      <AlertModal isOpen={isAlertModalOpen} isClosed={closeAlertModal} />
     </>
   );
 };
