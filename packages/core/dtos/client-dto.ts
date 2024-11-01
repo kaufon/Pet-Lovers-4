929266990db59f94e7e751f5eca0557a60553e39
kaufon <kauanfvale@gmail.com>
2024-11-01 18:58:37 -0300

    👥client: client dto

---
+++ packages/core/dtos/client-dto.ts
@@ -0,0 +1,11 @@
+import type { CellPhoneDto, LinkDto, AddressDto } from "./index";
+
+export type ClientDto = {
+  id?: number;
+  nome: string;
+  nomeSocial: string;
+  email: string | null;
+  endereco: AddressDto;
+  telefones: CellPhoneDto[];
+  links: LinkDto[];
+};
