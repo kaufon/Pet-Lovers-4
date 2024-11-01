067e124e1c4f55487441f5fc8f21f327293bf868
kaufon <kauanfvale@gmail.com>
2024-11-01 18:58:45 -0300

    👥entities: link dto

---
+++ packages/core/dtos/index.ts
@@ -0,0 +1,4 @@
+export type { AddressDto } from "./address-dto";
+export type { ClientDto } from "./client-dto";
+export type { CellPhoneDto } from "./cellphone-dto";
+export type { LinkDto } from "./link-dto";
---
+++ packages/core/dtos/link-dto.ts
@@ -0,0 +1,5 @@
+
+export type LinkDto = {
+  rel?: string 
+  href?: string
+}
