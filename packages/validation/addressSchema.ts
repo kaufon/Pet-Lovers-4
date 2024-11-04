5e180e1b1478b8c237540e283c16f1abfb017fcc
kaufon <kauanfvale@gmail.com>
2024-11-04 16:42:32 -0300

    🪨constants: address  schema

---
+++ packages/validation/addressSchema.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+import { stringSchema } from "./stringSchema";
+export const addressSchema = z.object({
+  estado: stringSchema,
+  cidade: stringSchema,
+  bairro: stringSchema,
+  rua: stringSchema,
+  numero: stringSchema,
+  codigoPostal: stringSchema.max(8,{message: "CEP inválido"}),
+  informacoesAdicionais: stringSchema.optional(),
+});
