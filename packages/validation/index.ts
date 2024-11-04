9b7c058ca64a4fc433114535efc33c38e0ae323a
kaufon <kauanfvale@gmail.com>
2024-11-04 16:43:48 -0300

    🪨constant: linkschema

---
+++ packages/validation/index.ts
@@ -0,0 +1,6 @@
+export { addressSchema } from "./addressSchema";
+export {stringSchema} from './stringSchema'
+export {emailSchema} from './emailSchema'
+export {nameSchema} from './nameSchema'
+export { LinkSchema } from "./linkSchema";
+export { cellPhoneSchema } from "./cellphoneschema";
